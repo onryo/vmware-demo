579bbd69ccec52b973d350adbd1eac7f86be1297
Chris Dunlap <chris@roadkill.org>
2019-05-01 14:00:08 -0700

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -9,3 +9,16 @@ variable "vsphere_password" {
 variable "vsphere_server" {
   description = "vsphere_server"
 }
+
+variable "var.dc_name" {
+  description = "DC name"
+}
+
+variable "var.datastore" {
+  description = "Datastore"
+}
+
+variable "var.vm_name" {
+  description = "Vm name"
+}
+
