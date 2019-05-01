4aec661c23224b5fbdc7f28954ce806db4e2023e
Chris Dunlap <chris@roadkill.org>
2019-05-01 14:25:06 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,21 +20,3 @@ data "vsphere_resource_pool" "pool" {
 }
 
 
-resource "vsphere_virtual_machine" "vm" {
-  name             = "${var.vm_name}"
-  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
-  datastore_id     = "${data.vsphere_datastore.datastore.id}"
-
-  num_cpus = 2
-  memory   = 1024
-  guest_id = "centos7_64Guest"
-
-  network_interface {
-    network_id = "${data.vsphere_network.network.id}"
-  }
-
-  disk {
-    label = "disk0"
-    size  = 20
-  }
-}
