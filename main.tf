95bb07675443d20f84dced66fde1523306dfc4c5
Chris Dunlap <chris@roadkill.org>
2019-05-01 13:18:05 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,22 @@
 provider "vsphere" {
   user           = "${var.vsphere_user}"
   password       = "${var.vsphere_password}"
-  vsphere_server = "${var.vsphere_server}"
+  vsphere_server = "${var.vsphere_server}"  
 
   # If you have a self-signed cert
   allow_unverified_ssl = true
+}
+
+variable "vsphere_user" {
+  description = "vsphere_user"
+}
+
+variable "vsphere_password" {
+  description = "vsphere_password"
+}
+
+variable "vsphere_server" {
+  description = "vsphere_server"
 }
 
 data "vsphere_datacenter" "dc" {
