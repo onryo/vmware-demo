1631c7e2b0bb30241d47611573039887b4ca3ac2
Chris Dunlap <chris@roadkill.org>
2019-06-19 13:33:17 -0700

    remove resource pool

--- vm.tf
+++ vm.tf
@@ -1,12 +1,12 @@
 data "vsphere_virtual_machine" "template_from_ovf" {
-name = "templatebeatles"
+name = "UbuntuTemplate"
 datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
 
 resource "vsphere_virtual_machine" "vm" {
   name             = "${var.vm_name}"
-  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
+#  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
   datastore_id     = "${data.vsphere_datastore.datastore.id}"
 clone {
     template_uuid = "${data.vsphere_virtual_machine.template_from_ovf.id}"
