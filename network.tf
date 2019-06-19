7908cf803a625ce4ba28fd8752dadca63e5a2067
Chris Dunlap <chris@roadkill.org>
2019-06-19 13:28:49 -0700

    changed network

--- network.tf
+++ network.tf
@@ -2,6 +2,6 @@ data "vsphere_datacenter" "dc" {
   name = "${var.dc_name}"
 }
 data "vsphere_network" "network" {
-  name          = "automation_test"
+  name          = "VM Network"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
