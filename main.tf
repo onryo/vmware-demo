aa5aa1a3e8ad6add613f46106fe5f659f480f357
Chris Dunlap <chris@roadkill.org>
2019-06-19 14:15:46 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,8 @@
-module "vsphere" {
+module "vsphere/" {
  
-  source = "vsphere"
-
+  source = "github.com/stoffee/vmware-demo/vsphere/"
+  name             = "${var.vm_name}"
+ resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
+#  resource_pool_id = "${var.pool}"
+  datastore_id     = "${data.vsphere_datastore.datastore.id}"
 }
