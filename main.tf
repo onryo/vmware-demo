c609edbcfcdf7cd3afd121d3ec1227937bf07817
stoffee <chris@roadkill.org>
2019-06-20 10:10:41 -0700

    removed modules

--- main.tf
+++ main.tf
@@ -1,8 +1,22 @@
-module "vsphere/" {
- 
-  source = "github.com/stoffee/vmware-demo/vsphere/"
-  name             = "${var.vm_name}"
- resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
-#  resource_pool_id = "${var.pool}"
-  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+provider "vsphere" {
+  user           = "${var.vsphere_user}"
+  password       = "${var.vsphere_password}"
+  vsphere_server = "${var.vsphere_server}"  
+
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
 }
+
+
+
+data "vsphere_datastore" "datastore" {
+  name          = "${var.datastore}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_resource_pool" "pool" {
+ name          = "${var.pool}"
+ datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+
